e793b3ed7736c2f27e0981aa0594dc7b5b72aa2f
aman <amansaini1407@gmail.com>
2024-08-03 01:15:25 +0530

    Check Profiler

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { TokenStorageService } from '../_services/token-storage.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
@@ -9,9 +10,13 @@ import { TokenStorageService } from '../_services/token-storage.service';
 export class ProfileComponent implements OnInit {
   currentUser: any;
 
-  constructor(private token: TokenStorageService) { }
+  constructor(private token: TokenStorageService , private router:Router) { }
 
   ngOnInit(): void {
     this.currentUser = this.token.getUser();
+    if(this.currentUser.get == null || this.currentUser == "")
+    {
+      this.router.navigateByUrl("/login");
+    }
   }
 }
