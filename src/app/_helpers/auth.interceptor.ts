56c3e8b9bc09ca200f7dd25ef8cafd7d0d5a4b80
aman <amansaini1407@gmail.com>
2025-04-18 01:27:20 +0530

    JWT Token Expiry Fixed

--- src/app/_helpers/auth.interceptor.ts
+++ src/app/_helpers/auth.interceptor.ts
@@ -1,15 +1,16 @@
-import { HTTP_INTERCEPTORS, HttpEvent } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpErrorResponse, HttpEvent } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { HttpInterceptor, HttpHandler, HttpRequest } from '@angular/common/http';
 import { TokenStorageService } from '../_services/token-storage.service';
-import { Observable } from 'rxjs';
+import { catchError, Observable, throwError } from 'rxjs';
+import { Route, Router } from '@angular/router';
 
 const TOKEN_HEADER_KEY = 'Authorization';       // for Spring Boot back-end
 // const TOKEN_HEADER_KEY = 'x-access-token';   // for Node.js Express back-end
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
-  constructor(private token: TokenStorageService) { }
+  constructor(private token: TokenStorageService , private router:Router) { }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     let authReq = req;
@@ -17,11 +18,19 @@ export class AuthInterceptor implements HttpInterceptor {
     if (token != null) {
       // for Spring Boot back-end
       authReq = req.clone({ headers: req.headers.set(TOKEN_HEADER_KEY, 'Bearer ' + token) });
-
-      // for Node.js Express back-end
-      // authReq = req.clone({ headers: req.headers.set(TOKEN_HEADER_KEY, token) });
     }
-    return next.handle(authReq);
+    // return next.handle(authReq); Pahle Ye Then validate karna ka liys Pipe use kiya hain
+    return next.handle(authReq).pipe(
+      catchError((err: HttpErrorResponse) => {
+        if (err.status === 401) {
+          // Token expired or unauthorized
+          this.token.signOut(); // Optional: clear token from storage
+          this.router.navigate(['/login']); // Redirect to login
+        }
+        return throwError(() => err);
+      })
+    );
+    
   }
 }
 
